fd82f9eefa2d2bfb2edd77565ef2ab50a8be2214
Eliyas <mohamedeliyas.eliyas@gmail.com>
2018-09-15 21:36:00 +0530

    signup form integrated

--- app/containers/Signup/SignupForm.js
+++ app/containers/Signup/SignupForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import Input from '@material-ui/core/Input';
+import { Input, Select, MenuItem } from '@material-ui/core';
 import InputLabel from '@material-ui/core/InputLabel';
 import Button from '@material-ui/core/Button';
 import { Formik } from 'formik';
@@ -9,7 +9,7 @@ import * as Yup from 'yup';
 
 /* eslint react/prop-types: 0 */
 /* eslint prettier/prettier: 0 */
-const SignupForm = ({ classes }) => (
+const SignupForm = ({ classes, handleSignUp }) => (
   <Formik
     initialValues={{
       name: '',
@@ -19,17 +19,16 @@ const SignupForm = ({ classes }) => (
       passwordConfirm: '',
     }}
     validationSchema={Yup.object().shape({
-      name: Yup.string().required,
+      name: Yup.string().required('please provide name'),
       email: Yup.string()
         .email('please provide a valid email')
         .required('Please provide email'),
-      gender: Yup.string().oneOf[('male', 'female')],
       password: Yup.string().required('please provide passoword'),
       passwordConfirm: Yup.string()
-        .oneOf([Yup.ref('password'), null])
+        .oneOf([Yup.ref('password')], "Passwords don't match")
         .required('Password confirm is required'),
     })}
-    onSubmit={(values, actions) => console.log(values, actions)}
+    onSubmit={(values, actions) => handleSignUp(values, actions)}
   >
     {props => {
       const {
@@ -47,6 +46,24 @@ const SignupForm = ({ classes }) => (
           onSubmit={handleSubmit}
         >
           <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="name">Name</InputLabel>
+            <Input
+              id="name"
+              name="name"
+              autoComplete="name"
+              value={values.name}
+              onChange={handleChange}
+              autoFocus
+            />{' '}
+            {touched.name &&
+              errors.name && (
+              <FormHelperText className={classes.error}>
+                {errors.name}
+              </FormHelperText>
+            )}
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
             <InputLabel htmlFor="email">Email</InputLabel>
             <Input
               id="email"
@@ -63,6 +80,60 @@ const SignupForm = ({ classes }) => (
               </FormHelperText>
             )}
           </FormControl>
+
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="gender">Gender</InputLabel>
+            <Select
+              value={values.gender}
+              onChange={handleChange}
+              input={<Input id="gender" name="gender" />}
+            >
+              <MenuItem value="male">Male</MenuItem>
+              <MenuItem value="female">Female</MenuItem>
+              <MenuItem value="other">other</MenuItem>
+            </Select>
+            {touched.gender &&
+              errors.gender && (
+              <FormHelperText className={classes.error}>
+                {errors.gender}
+              </FormHelperText>
+            )}
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              value={values.password}
+              onChange={handleChange}
+            />{' '}
+            {touched.password &&
+              errors.password && (
+              <FormHelperText className={classes.error}>
+                {errors.password}
+              </FormHelperText>
+            )}
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="passwordConfirm">Password</InputLabel>
+            <Input
+              name="passwordConfirm"
+              type="password"
+              id="passwordConfirm"
+              value={values.passwordConfirm}
+              onChange={handleChange}
+            />{' '}
+            {touched.passwordConfirm &&
+              errors.passwordConfirm && (
+              <FormHelperText className={classes.error}>
+                {errors.passwordConfirm}
+              </FormHelperText>
+            )}
+          </FormControl>
+
           <Button
             fullWidth
             type="submit"
@@ -72,7 +143,7 @@ const SignupForm = ({ classes }) => (
             disabled={isSubmitting}
           >
             {' '}
-            Login
+            Signp
           </Button>
         </form>
       );
