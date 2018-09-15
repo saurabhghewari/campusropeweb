691adbba15ed247d160dc6e629b13d6cead304ab
umarfarooq95 <mohamedumar931@gmail.com>
2018-09-15 14:38:06 +0530

    implemented mobile bottom navigation ui

--- app/containers/Home/MobileNavBar.js
+++ app/containers/Home/MobileNavBar.js
@@ -12,23 +12,72 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
-const styles = {
-  grow: {
-    flexGrow: 1,
+import SearchIcon from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-around',
   },
-  hamburgerIcon: {},
-};
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '30%',
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+});
 
 function MobileNavbar(props) {
   const { classes } = props;
   return (
-    <div className={classes.grow}>
+    <div>
       <AppBar position="static">
-        <Toolbar variant="dense">
-          <Typography variant="title" className={classes.grow} color="inherit">
+        <Toolbar variant="dense" className={classes.root}>
+          <Typography variant="title" color="inherit">
             Campusrope
           </Typography>
+
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <Input
+              placeholder="Search…"
+              disableUnderline
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
           <IconButton
             className={classes.hamburgerIcon}
             color="inherit"
--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -18,8 +18,9 @@ import reducer from './reducer';
 import saga from './saga';
 
 // import Drawer from 'components/Drawer'
-// import MobileNavBar from './MobileNavBar';
-import BrowserNavbar from './BrowserNavbar';
+import MobileNavBar from './MobileNavBar';
+import AppBottomNavigation from './BottomNavigation';
+// import BrowserNavbar from './BrowserNavbar';
 
 /* eslint-disable react/prefer-stateless-function */
 export class Home extends React.Component {
@@ -30,7 +31,8 @@ export class Home extends React.Component {
           <title>Home</title>
           <meta name="description" content="Description of Home" />
         </Helmet>
-        <BrowserNavbar />
+        <MobileNavBar />
+        <AppBottomNavigation />
       </div>
     );
   }
