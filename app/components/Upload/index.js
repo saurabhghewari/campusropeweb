915f233e77869c8fa7b7a698b600bb9c571d15da
faiyazuddeen <m.faiyaz123@gmail.com>
2018-09-28 22:57:14 +0530

    cloudinary upload widget and async user search

--- app/components/Upload/index.js
+++ app/components/Upload/index.js
@@ -5,23 +5,55 @@
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import { withStyles } from '@material-ui/core/styles';
 
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
+  },
+  iconSmall: {
+    fontSize: 20,
+  },
+});
 
 /* eslint-disable react/prefer-stateless-function */
 class Upload extends React.Component {
+  onUploadOpen = () => {
+    window.cloudinary.openUploadWidget(
+      { cloud_name: 'demo', upload_preset: 'a5vxnzbp' },
+      (error, result) => {
+        console.log(error, result);
+      },
+    );
+  };
+
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <FormattedMessage {...messages.header} />
-      </div>
+      <Button
+        variant="contained"
+        color="default"
+        className={classes.button}
+        onClick={this.onUploadOpen}
+      >
+        Upload
+        <CloudUploadIcon className={classes.rightIcon} />
+      </Button>
     );
   }
 }
 
-Upload.propTypes = {};
+Upload.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-export default Upload;
+export default withStyles(styles)(Upload);
