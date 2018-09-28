9ff79d6a10aa7b7954b870e708ace7f32fec43d2
faiyazuddeen <m.faiyaz123@gmail.com>
2018-09-28 22:07:18 +0530

    created files for upload and user search component

---
+++ app/components/UserSearch/messages.js
@@ -0,0 +1,14 @@
+/*
+ * UserSearch Messages
+ *
+ * This contains all the text for the UserSearch component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.UserSearch.header',
+    defaultMessage: 'This is the UserSearch component !',
+  },
+});
