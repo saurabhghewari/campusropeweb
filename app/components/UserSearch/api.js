ee13534345858b2b66e0318d6a6bd849f2619d89
faiyaz <m.faiyaz123@gmail.com>
2018-10-09 20:55:41 +0530

    upgraded babel eslint to fix indent issue

--- app/components/UserSearch/api.js
+++ app/components/UserSearch/api.js
@@ -1,9 +1,8 @@
-import axios from 'axios';
 import { USER_URL } from '../../constants/urlconstants';
 import { getAxiosInstance } from '../../setup_axios';
 
 export const searchUser = term => {
-  const SEARCH_URL = USER_URL + '/search_user';
+  const SEARCH_URL = `${USER_URL}/search_user`;
   return getAxiosInstance()
     .post(SEARCH_URL, { term })
     .then(res => res.data);
