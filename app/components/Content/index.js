6d5075a259e7ac1f040c7369208c3cd59321d7ea
umarfarooq95 <mohamedumar931@gmail.com>
2018-10-13 11:06:00 +0530

    implemented content component

--- app/components/Content/index.js
+++ app/components/Content/index.js
@@ -5,23 +5,44 @@
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    minHeight: '70vh',
+    marginTop: theme.spacing.unit * 3,
+  },
+});
 
 /* eslint-disable react/prefer-stateless-function */
 class Content extends React.Component {
   render() {
+    const { classes, children } = this.props;
     return (
-      <div>
-        <FormattedMessage {...messages.header} />
-      </div>
+      <Grid container spacing={24}>
+        <Grid item lg={2} />
+        <Grid item xs={12} lg={8}>
+          <Paper className={classes.root} elevation={1}>
+            {children}
+          </Paper>
+        </Grid>
+        <Grid item lg={2} />
+      </Grid>
     );
   }
 }
 
-Content.propTypes = {};
+Content.propTypes = {
+  classes: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
-export default Content;
+export default withStyles(styles)(Content);
