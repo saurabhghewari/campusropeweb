b5bd5fcb5c0bd157ae3b07caab9fb0f4a0d5b8e1
faiyaz <m.faiyaz123@gmail.com>
2018-11-23 19:35:18 +0530

    generated dynamic list component for user profile

---
+++ app/components/DynamicList/messages.js
@@ -0,0 +1,14 @@
+/*
+ * DynamicList Messages
+ *
+ * This contains all the text for the DynamicList component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.DynamicList.header',
+    defaultMessage: 'This is the DynamicList component !',
+  },
+});
