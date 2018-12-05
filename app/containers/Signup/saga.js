57ac9ad4e5dd76b541b97564bd1c801afe1d4dee
faiyaz <m.faiyaz123@gmail.com>
2018-12-05 22:21:21 +0530

    signup featherized

--- app/containers/Signup/saga.js
+++ app/containers/Signup/saga.js
@@ -1,15 +1,31 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import { replace } from 'react-router-redux';
 import { SIGNUP_SUBMIT, EMAIL_ALREADY_EXISTS_MESSGE } from './constants';
-import { signUpApi } from './api';
 import { storeToken } from '../Login/saga';
+import { setLoggedUser } from '../../store/loggeduser/actions';
+import { fetchConstants } from '../../store/constants/actions';
+import feathersClient, { userService } from './../../feathers';
 
 export function* submitSignupDetails({ values, actions }) {
   const { resetForm, setErrors, setSubmitting } = actions;
   try {
-    const response = yield call(signUpApi, values);
+    yield userService.create(values);
+    const response = yield call(feathersClient.authenticate, {
+      strategy: 'local',
+      email: values.email,
+      password: values.password,
+    });
+
+    const payload = yield feathersClient.passport.verifyJWT(
+      response.accessToken,
+    );
+    const loggedUser = yield userService.get(payload.userId);
+    feathersClient.set('user', loggedUser);
+    // Reset the form just to be clean, then send the user to our home  which "requires" authentication
     yield call(resetForm);
-    yield call(storeToken, response.data.token);
+    yield put(setLoggedUser(loggedUser));
+    yield put(fetchConstants());
+    yield call(storeToken, response.accessToken);
     yield put(replace('/app'));
   } catch (e) {
     if (e.response.status === 409) {
